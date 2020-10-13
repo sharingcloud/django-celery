189da691e2e589400486fab225834ca7848074b2
Denis BOURGE <denis.bourge@sharingcloud.com>
2020-10-13 14:47:34 +0000

    feat: celery 4 support

--- djcelery/management/commands/celerycam.py
+++ djcelery/management/commands/celerycam.py
@@ -5,6 +5,7 @@ Shortcut to the Django snapshot service.
 """
 from __future__ import absolute_import, unicode_literals
 
+from celery import VERSION as CELERY_VERSION
 from celery.bin import events
 
 from djcelery.app import app
@@ -16,13 +17,22 @@ ev = events.events(app=app)
 class Command(CeleryCommand):
     """Run the celery curses event viewer."""
     help = 'Takes snapshots of the clusters state to the database.'
-    options = (
-        tuple(CeleryCommand.options) +
-        tuple(ev.get_options()) +
-        tuple(getattr(ev, 'preload_options', ()))
-    )
+    if CELERY_VERSION[0] < 4:
+        options = (
+            tuple(CeleryCommand.options) +
+            tuple(ev.get_options()) +
+            tuple(getattr(ev, 'preload_options', ()))
+        )
+    else:
+        def add_arguments(self, parser):
+            super().add_arguments(parser)
+            ev.add_arguments(parser)
+
+            # Deprecated args
+            parser.add_argument("--workdir", help="deprecated")
 
     def handle(self, *args, **options):
         """Handle the management command."""
-        options['camera'] = 'djcelery.snapshot.Camera'
+        if not options["camera"]:
+            options['camera'] = 'djcelery.snapshot.Camera'
         ev.run(*args, **options)
